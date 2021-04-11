1b0f411b3ea1e1fd5f6b00d7041ced0213d561e9
murlocmanx <filip.stamenkovic7@gmail.com>
2021-04-11 02:02:29 +0200

    skoro gotovo, samo jos delete

--- PracticeWork1/src/Note.ts
+++ PracticeWork1/src/Note.ts
@@ -1,16 +1,42 @@
+import { createHtmlElement } from './NoteApp';
+
 export class Note {
+    public id: number;
     public title :string;
     public text :string;
-    public date :string;
-    constructor(title :string, text :string, date :string) {
+    constructor(id: number, title :string, text :string) {
         this.title=title;
         this.text=text;
-        this.date=date;
-
+        this.id=id;
     }
 
-    printInfo() {
-        console.log(`Title: ${this.title} , Text: ${this.text}`)
+    renderNote(host :HTMLElement) {
+        let noteListContainer =<HTMLElement> createHtmlElement(host, "li", "noteListContainer mdl-list__item mdl-list__item--three-line");
+
+        let noteSpanContainer = <HTMLElement> createHtmlElement(noteListContainer, "span", "noteSpanContainer mdl-list__item-primary-content");
+
+        let noteTitleSpan = <HTMLElement>createHtmlElement(noteSpanContainer, "span", "noteTitleSpan");
+        noteTitleSpan.innerHTML=this.title;
+
+        let noteTextSpan = <HTMLElement>createHtmlElement(noteSpanContainer, "span", "noteTextSpan mdl-list__item-text-body");
+        noteTextSpan.innerHTML=this.text;
+
+        let notebuttonSpan = <HTMLElement>createHtmlElement(noteListContainer, "span", "notebuttonSpan mdl-list__item-secondary-content");
+
+        let notebuttonDelete = <HTMLElement>createHtmlElement(notebuttonSpan, "button", "notebuttonDelete mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-button--colored mdl-js-ripple-effect");
+        notebuttonDelete.onclick = () => {
+            dispatchEvent(deleteEvent);
+        };
+
+        let notebuttonDeleteIcon = <HTMLElement>createHtmlElement(notebuttonDelete, "i", "notebuttonDeleteIcon material-icons");
+        notebuttonDeleteIcon.innerHTML="delete";
+
+        var deleteEvent = new CustomEvent("delete", {
+            detail: {
+                noteID: this.id
+              }
+        });
+        
     }
 
 }
--- PracticeWork1/src/NoteApp.ts
+++ PracticeWork1/src/NoteApp.ts
@@ -3,78 +3,99 @@ import { Note } from "./Note";
 
 export class NoteApp {
     public noteList: Array<Note>;
+    public noteCounter: number;
 
     constructor() {
         this.noteList = [];
+        this.noteCounter = 0;
     }
 
     renderNoteApp(body: HTMLElement) {
-        createHtmlElement(body, "div", "mainAppContainerDiv");
-        let mainAppContainerDiv = <HTMLElement>document.querySelector(".mainAppContainerDiv");
+        let mainAppContainerDiv = <HTMLElement>createHtmlElement(body, "div", "mainAppContainerDiv");
 
-        createHtmlElement(mainAppContainerDiv, "div", "noteAppInputDiv");
-        let noteAppInputDiv = <HTMLElement>document.querySelector(".noteAppInputDiv");
+        let noteAppInputDiv = <HTMLElement>createHtmlElement(mainAppContainerDiv, "div", "noteAppInputDiv");
 
-        createHtmlElement(mainAppContainerDiv, "div", "noteAppNoteListDiv");
-        let noteAppNoteListDiv = <HTMLElement>document.querySelector(".noteAppNoteListDiv");
+        let noteAppNoteListDiv = <HTMLElement>createHtmlElement(mainAppContainerDiv, "div", "noteAppNoteListDiv");
 
         this.renderNoteInput(noteAppInputDiv);
-    
+        this.renderNoteList(noteAppNoteListDiv);
+
     }
 
     renderNoteInput(host: HTMLElement) {
-        createHtmlElement(host, "span", "InputHead mdl-typography--display-2");
-        let noteInputHead = <HTMLElement>document.querySelector(".InputHead");
+        let noteInputHead = <HTMLElement>createHtmlElement(host, "span", "InputHead mdl-typography--display-2");
         noteInputHead.innerHTML = "Add your note :)";
 
-        createHtmlElement(host, "form", "InputForm");
-        let noteInputForm = <HTMLElement>document.querySelector(".InputForm");
+        let noteInputForm = <HTMLElement>createHtmlElement(host, "form", "InputForm");
+        noteInputForm.setAttribute("onsubmit", "return false;");
 
-        createHtmlElement(noteInputForm, "div", "containerInputFieldsDiv");
-        let containerInputFieldsDiv = <HTMLElement>document.querySelector(".containerInputFieldsDiv");
+        let containerInputFieldsDiv = <HTMLElement>createHtmlElement(noteInputForm, "div", "containerInputFieldsDiv");
 
-        createHtmlElement(containerInputFieldsDiv, "div", "TitleInputDiv mdl-textfield mdl-js-textfield");
-        let noteInputTitleInputDiv = <HTMLElement>document.querySelector(".TitleInputDiv");
+        let noteInputTitleInputDiv = <HTMLElement>createHtmlElement(containerInputFieldsDiv, "div", "TitleInputDiv mdl-textfield mdl-js-textfield");
 
-        createHtmlElement(noteInputTitleInputDiv, "input", "TitleInputField mdl-textfield__input");
-        let titleInputField = <HTMLElement>document.querySelector(".TitleInputField");
+        let titleInputField = <HTMLElement>createHtmlElement(noteInputTitleInputDiv, "input", "TitleInputField mdl-textfield__input");
         titleInputField.id = "title1";
 
-        createHtmlElement(noteInputTitleInputDiv, "label", "TitleInputLabel mdl-textfield__label");
-        let titleInputLabel = <HTMLElement>document.querySelector(".TitleInputLabel");
+        let titleInputLabel = <HTMLElement>createHtmlElement(noteInputTitleInputDiv, "label", "TitleInputLabel mdl-textfield__label");
         titleInputLabel.setAttribute("type", "text");
         titleInputLabel.setAttribute("for", "title1");
-        titleInputLabel.innerHTML = "Title";
+        titleInputLabel.innerHTML = "Note title";
 
 
-        createHtmlElement(containerInputFieldsDiv, "div", "TextInputDiv mdl-textfield mdl-js-textfield");
-        let noteInputTextInputDiv = <HTMLElement>document.querySelector(".TextInputDiv");
+        let noteInputTextInputDiv = <HTMLElement>createHtmlElement(containerInputFieldsDiv, "div", "TextInputDiv mdl-textfield mdl-js-textfield");
 
-        createHtmlElement(noteInputTextInputDiv, "textarea", "TextInputField mdl-textfield__input");
-        let textInputField = <HTMLElement>document.querySelector(".TextInputField");
+        let textInputField = <HTMLElement>createHtmlElement(noteInputTextInputDiv, "textarea", "TextInputField mdl-textfield__input");
         textInputField.setAttribute("rows", "8");
         textInputField.id = "text1";
 
-        createHtmlElement(noteInputTextInputDiv, "label", "TextInputLabel mdl-textfield__label");
-        let textInputLabel = <HTMLElement>document.querySelector(".TextInputLabel");
+        let textInputLabel = <HTMLElement>createHtmlElement(noteInputTextInputDiv, "label", "TextInputLabel mdl-textfield__label");
         textInputLabel.setAttribute("type", "text");
         textInputLabel.setAttribute("for", "text1");
         textInputLabel.innerHTML = "Note text";
 
-        createHtmlElement(containerInputFieldsDiv, "div", "buttonDiv");
-        let buttonDiv = <HTMLElement>document.querySelector(".buttonDiv");
+        let buttonDiv = <HTMLElement>createHtmlElement(containerInputFieldsDiv, "div", "buttonDiv");
 
-        createHtmlElement(buttonDiv, "button", "buttonAdd mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--primary");
-        let buttonAdd = <HTMLElement>document.querySelector(".buttonAdd");
+        let buttonAdd = <HTMLElement>createHtmlElement(buttonDiv, "button", "buttonAdd mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--primary");
+        buttonAdd.onclick = () => {
+            this.handleNoteSubmit();
+            (<HTMLInputElement>titleInputField).value="";
+            (<HTMLInputElement>textInputField).value="";
+        }
 
-        createHtmlElement(buttonAdd, "i", "buttonIcon material-icons");
-        let buttonIcon = <HTMLElement>document.querySelector(".buttonIcon");
-        buttonIcon.innerHTML="add";
+        let buttonIcon = <HTMLElement>createHtmlElement(buttonAdd, "i", "buttonIcon material-icons");
+        buttonIcon.innerHTML = "add";
+        
+       
+    }
 
+    renderNoteList(host: HTMLElement) {
+        let noteListUnorderdList = <HTMLElement>createHtmlElement(host, "ul", "noteListUnorderdList demo-list-three mdl-list");
+        this.noteList.forEach(note => {
+            note.renderNote(noteListUnorderdList);
+        });
 
-
+        addEventListener("delete", function(e) { this.handleDeleteEvent((<CustomEvent>e).detail.id); });
 
     }
+
+    handleNoteSubmit(){
+        this.noteCounter++;
+        let title =(<HTMLInputElement>document.getElementById("title1")).value;
+        let text= (<HTMLInputElement>document.getElementById("text1")).value;
+        this.noteList.push(new Note(this.noteCounter,<string>title,<string>text));
+        let noteAppNoteListDiv = <HTMLElement>document.querySelector(".noteAppNoteListDiv")
+        noteAppNoteListDiv.innerHTML='';
+        this.renderNoteList(noteAppNoteListDiv);
+    }
+
+    handleDeleteEvent(id: number) {
+        this.noteList=this.noteList.filter(note => note.id !==id);
+        let noteAppNoteListDiv = <HTMLElement>document.querySelector(".noteAppNoteListDiv")
+        noteAppNoteListDiv.innerHTML='';
+        this.renderNoteList(noteAppNoteListDiv);
+    }
+
+   
 
 }
 
@@ -83,4 +104,5 @@ export function createHtmlElement(host: HTMLElement, element: string, className:
     let childElement = document.createElement(element);
     childElement.className = className;
     hostElement.appendChild(childElement);
+    return childElement;
 }
