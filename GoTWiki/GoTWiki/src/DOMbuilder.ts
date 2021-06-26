6dac24820d73ba86606d2427f7ac9731543ce848
FilipStam97 <49717071+FilipStam97@users.noreply.github.com>
2021-06-26 15:48:32 +0200

    sitnice

--- GoTWiki/GoTWiki/src/DOMbuilder.ts
+++ GoTWiki/GoTWiki/src/DOMbuilder.ts
@@ -1,9 +1,8 @@
 
 export function createHtmlElement(host: HTMLElement, element: string, className: string) {
-    let hostElement = host;
     let childElement = document.createElement(element);
     childElement.className = className;
-    hostElement.appendChild(childElement);
+    host.appendChild(childElement);
     return <HTMLElement>childElement;
 }
 
--- GoTWiki/GoTWiki/src/WikiApp.ts
+++ GoTWiki/GoTWiki/src/WikiApp.ts
@@ -112,5 +112,8 @@ const headerContentList = [
     {
         title: "Locations"
     },
+    {
+        title: "Episode Transcripts"
+    },
 
 ]
