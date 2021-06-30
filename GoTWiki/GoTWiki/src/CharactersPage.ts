d35a5d519c5d89c4db352c7e24cbcb5aecf70248
FilipStam97 <49717071+FilipStam97@users.noreply.github.com>
2021-06-30 22:40:19 +0200

    filter u sidenav

--- GoTWiki/GoTWiki/src/CharactersPage.ts
+++ GoTWiki/GoTWiki/src/CharactersPage.ts
@@ -1,4 +1,5 @@
-import { createHtmlElement } from "./DOMbuilder";
+import { identity } from "rxjs";
+import { createHtmlElement, FILTER_CONST, renderCheckBox } from "./DOMbuilder";
 
 export function renderCharactersPage(host: HTMLElement){
 
@@ -11,7 +12,8 @@ export function renderCharactersPage(host: HTMLElement){
 }
 
 export function renderSideNav(host: HTMLElement){
-    host.innerHTML="BLLLLLLLLLLLLLLLLLLAAA";
+    renderCheckboxCategories(host, FILTER_CONST.elementArray);
+   // host.innerHTML="BLLLLLLLLLLLLLLLLLLAAA";
 
 }
 
@@ -19,4 +21,51 @@ export function renderCharactersList(host: HTMLElement) {
     host.innerHTML="ZLAAAAAAAAAAAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
 
+}
+
+export function renderCheckboxCategories(host: HTMLElement, categorieList: Array<any>){
+    categorieList.forEach(cetegorie => {
+        let cetegorieDiv = createHtmlElement(host,"div", "cetegorieDiv");
+        let categorieTitle = createHtmlElement(cetegorieDiv,"span", "categorieTitle");
+        categorieTitle.innerHTML= cetegorie.title;
+
+        let categorieElementsWrapperDiv = createHtmlElement(cetegorieDiv, "div", "cetegorieDiv");
+        let categorieElementsCollapseDiv = createHtmlElement(cetegorieDiv, "div", "categorieElementsCollapseDiv collapse mt-3");
+        categorieElementsCollapseDiv.id=cetegorie.name;
+
+        if(cetegorie.values.length > 4) {
+
+        let anchorCollapseButton = <HTMLAnchorElement>createHtmlElement(cetegorieDiv, "a", "anchorCollapseButton");
+        anchorCollapseButton.setAttribute("data-mdb-toggle", "collapse");
+        anchorCollapseButton.href=`#${cetegorie.name}`;
+        anchorCollapseButton.setAttribute("aria-expanded","false");
+        anchorCollapseButton.setAttribute("aria-controls",cetegorie.name);
+        anchorCollapseButton.setAttribute("role", "button");
+        
+        let anchorIcon =<HTMLImageElement> createHtmlElement(anchorCollapseButton,"i","anchorIcon fas fa-angle-down");
+        anchorIcon.innerHTML="Show more";
+        anchorCollapseButton.onclick =()=> {
+            if(anchorCollapseButton.getAttribute("aria-expanded")=="true") {
+                anchorIcon.className="fas fa-angle-up";
+                anchorIcon.innerHTML="Show less";
+            }
+            else{
+                anchorIcon.className="fas fa-angle-down";
+                anchorIcon.innerHTML="Show more";
+            } 
+        }
+   
+        }
+        cetegorie.values.sort().forEach((value: string, index: number) => {
+            if(index<4){
+                renderCheckBox(categorieElementsWrapperDiv,value,cetegorie.name);
+            }
+            else{
+                renderCheckBox(categorieElementsCollapseDiv,value,cetegorie.name);
+
+            }
+           
+        });
+
+    });
 }
--- GoTWiki/GoTWiki/src/DOMbuilder.ts
+++ GoTWiki/GoTWiki/src/DOMbuilder.ts
@@ -6,10 +6,170 @@ export function createHtmlElement(host: HTMLElement, element: string, className:
     return <HTMLElement>childElement;
 }
 
+export function renderCheckBox(host: HTMLElement, checkBoxItem : string, checkBoxGroupName: string) {
+    let checkBoxDiv = createHtmlElement(host,"div", "checkBoxDiv form-check");
+    let checkBoxInput = <HTMLInputElement>createHtmlElement(checkBoxDiv,"input", "checkBoxInput form-check-input");
+    checkBoxInput.type="checkbox";
+    checkBoxInput.value=checkBoxItem;
+    checkBoxInput.id=checkBoxItem+checkBoxGroupName;
+    let checkBoxLabel = <HTMLLabelElement>createHtmlElement(checkBoxDiv,"label", "checkBoxLabel form-check-label");
+    checkBoxLabel.setAttribute("for", checkBoxItem+checkBoxGroupName);
+    checkBoxLabel.innerHTML= checkBoxItem;
+
+}
 
 
 
 
-
+export const FILTER_CONST = {
+   elementArray: [
+        {
+            title: "Royalty",
+            name: "royal",
+            values : [
+                "Royalty",
+                "Not royalty"
+            ]
+        },
+        {
+            title: "House Name",
+            name: "houseName",
+            values : [
+                "Targaryen",
+                "Greyjoy",
+                "Lannister",
+                "Stark",
+                "Frey",
+                "Tully",
+                "Baratheon",
+                "Martell",
+                "Mormont",
+                "Tyrell",
+                "Arryn",
+                "Umber",
+                "Bolton",
+                "Tarly",
+            ]
+        },
+        {
+            title: "Killed By",
+            name: "killedBy",
+            values : [
+                "Gregor Clegane",
+                "Jaime Lannister",
+                "Daario Naharis",
+                "Jon Snow",
+                "Jaqen H'ghar",
+                "Tyene Sand",
+                "Howland Reed",
+                "Eddard Stark",
+                "Euron Greyjoy",
+                "Sons of the Harpy",
+                "Wights",
+                "Sandor Clegane",
+                "Arya Stark",
+                "Aerys II Targaryen",
+                "Lem Lemoncloak",
+                "Black Walder Rivers",
+                "Summer",
+                "Roof collapse",
+                "Karl Tanner",
+                "Daenerys Targaryen",
+                "Night's Watchman",
+                "Petyr Baelish",
+                "Ellaria Sand",
+                "Osha",
+                "Ilyn Payne",
+                "Wight",
+                "White Walker",
+                "Robert Baratheon",
+                "Little Birds",
+                "Unsullied",
+                "Mag the Mighty",
+                "Frey Crossbowmen",
+                "Grey Worm",
+                "Cersei Lannister",
+                "Doreah",
+                "Rast",
+                "Olenna Tyrell",
+                "Meera Reed",
+                "Lysa Arryn",
+                "Alliser Thorne",
+                "Othell Yarwyck",
+                "Bowen Marsh",
+                "Olly",
+                "Catelyn Stark",
+                "Pack of Wights",
+                "Wildling",
+                "Drogon",
+                "Mountain Clansman",
+                "The Waif",
+                "Bronn",
+                "Hodor",
+                "Polliver",
+                "Wun Wun",
+                "Old Age",
+                "Maester Cressen",
+                "Grenn",
+                "Donnel Hill",
+                "Cooper",
+                "Khal Drogo",
+                "Melisandre",
+                "Podrick Payne",
+                "Rickard Karstark",
+                "Obara Sand",
+                "Theon Greyjoy",
+                "The Night King",
+                "Ramsay Snow",
+                "Viserion",
+                "Rhaegal",
+                "Ygritte",
+                "Jorah Mormont",
+                "Khal Jhaqo's Khalasar",
+                "Ramsay Snow's Hounds",
+                "Ghost",
+                "Tormund Giantsbane",
+                "Mirri Maz Duur",
+                "Robb Stark",
+                "Roose Bolton",
+                "Boar",
+                "Joffrey Baratheon",
+                "Selyse Baratheon",
+                "Lannister Guardsman",
+                "Tyrion Lannister",
+                "Smalljon Umber",
+                "Pyat Pree",
+                "Brienne of Tarth",
+                "Meryn Trant",
+                "Lothar Frey",
+                "Samwell Tarly",
+                "Tommen Baratheon",
+                "Goldcloak",
+                "Lyanna Mormont",
+                "Amory Lorch"
+            ]
+        },
+        {
+            title: "Serves",
+            name: "serves",
+            values : [
+            "Doran Martell",
+            "Aerys II Targaryen",
+            "Daenerys Targaryen",
+            "Catelyn Stark",
+            "Sansa Stark",
+            "Bran Stark",
+            "Stannis Baratheon",
+            "Jon Snow",
+            "Tywin Lannister",
+            "Cersei Lannister",
+            "Eddard Stark",
+            "Tyrion Lannister",
+            "Brienne of Tarth",
+            ]
+        },
+        
+    ]
+}
 
 
